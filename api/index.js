40614a8c38e93c8b6ad6c807fbd25a62597b4eb4
chintan-27 <chintan.acharya27@gmail.com>
2026-01-23 13:15:11 -0500

    fix: increase body size limit to 50mb for sync endpoints

--- api/index.js
+++ api/index.js
@@ -39,8 +39,8 @@ app.use(helmet({
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 // Serve static files from public directory
 app.use('/static', express.static(path.join(__dirname, 'public')));
